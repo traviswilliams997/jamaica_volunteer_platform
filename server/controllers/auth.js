6670cf33d6f75de31a26fa51b4e7826dfab720aa
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-02-25 18:25:58 -0500

    Create auth for agency
    - [x] Created loginAgency and register agency functions
    - [x] Created routes
    - [x] Fixed imports in models

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,8 +2,9 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { SECRET } from '../utils/config.js'
 import Volunteer from '../models/volunteer.js'
+import Agency from '../models/agency.js'
 
-/* REGISTER VOLUNTEER*/
+/* REGISTER */
 export const registerVolunteer = async (req, res) => {
   try {
     const {
@@ -48,13 +49,58 @@ export const registerVolunteer = async (req, res) => {
   }
 }
 
-/* LOGGING IN */
+export const registerAgency = async (req, res) => {
+  try {
+    const {
+      username,
+      name,
+      email,
+      phoneNumber,
+      password,
+      type,
+      picturePath,
+      latitude,
+      longitude,
+      about,
+      admin,
+    } = req.body
+
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const newAgency = new Agency({
+      username,
+      name,
+      email,
+      phoneNumber,
+      password: passwordHash,
+      type,
+      picturePath,
+      latitude,
+      longitude,
+      about,
+      admin,
+    })
+
+    const savedAgency = await newAgency.save()
+    res.status(201).json(savedAgency)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+/* LOGGING  */
 
 export const loginVolunteer = async (req, res) => {
   try {
     const { username, password } = req.body
 
-    const volunteer = await Volunteer.findOne({ username: username })
+    const volunteer = await Volunteer.findOne({
+      where: {
+        username: username,
+      },
+    })
 
     const passwordCorrect =
       volunteer === null
@@ -76,6 +122,39 @@ export const loginVolunteer = async (req, res) => {
     delete volunteer.password
 
     res.status(200).send({ token, username: volunteer.username })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+export const loginAgency = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    const agency = await Agency.findOne({
+      where: {
+        username: username,
+      },
+    })
+
+    const passwordCorrect =
+      agency === null ? false : await bcrypt.compare(password, agency.password)
+
+    if (!(agency && passwordCorrect)) {
+      return res.status(401).json({
+        error: 'invalid username  or password',
+      })
+    }
+
+    const agencyForToken = {
+      username: agency.username,
+      id: agency.id,
+    }
+
+    const token = jwt.sign(agencyForToken, SECRET)
+    delete agency.password
+
+    res.status(200).send({ token, username: agency.username })
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
