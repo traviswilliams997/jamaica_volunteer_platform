2d88acdcf35ad1db1e7a3fbc70f976eb11ac2895
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-02-26 22:53:26 -0500

    Frontend and redux setup
    - [x] Setup redux and reducer
    - [x] Setup redux persist
    - [x] Create intial folder structer
    - [x] Deleted necessary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+import store from './state/store'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
