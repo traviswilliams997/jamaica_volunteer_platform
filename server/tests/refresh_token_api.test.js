df8e4609d5e2aef36e0630f1c0ca0c4fb5ba97c3
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-02-28 16:20:54 -0500

    Add refresh agency access token tests

--- server/tests/refresh_token_api.test.js
+++ server/tests/refresh_token_api.test.js
@@ -72,6 +72,62 @@ describe('Volunteer refresh token api', () => {
   })
 })
 
+describe('Agency refresh token api', () => {
+  beforeEach(async () => {
+    await emptyDbTables(['Agency', 'AgencyToken'])
+  })
+
+  test('Access token is refreshed', async () => {
+    const newAgency = {
+      username: 'helpthepoor',
+      name: 'helpthepoor',
+      email: 'helpthepoor@gmail.com',
+      phoneNumber: '(876)444-5555',
+      password: 'secret',
+      type: 'donations',
+      picturePath: '',
+      latitude: 18.0059,
+      longitude: -76.7468,
+      about: 'we help the needy',
+      admin: false,
+    }
+
+    await api
+      .post('/api/auth/register/agency')
+      .send(newAgency)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const loginCredentials = {
+      email: 'helpthepoor@gmail.com',
+      password: 'secret',
+    }
+
+    const loginReponse = await api
+      .post('/api/auth/login/agency')
+      .send(loginCredentials)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const originalAccessToken = loginReponse.body.accessToken
+
+    const tokensAfterLogin = await agencyTokensInDb()
+    const refreshToken = tokensAfterLogin[0].token
+
+    const refreshTokenResponse = await api
+      .get('/api/refresh/agency')
+      .set('Cookie', `jwt=${refreshToken}`)
+      .expect(200)
+
+    const newToken = refreshTokenResponse.body.accessToken
+
+    assert.notStrictEqual(originalAccessToken, newToken)
+  })
+  after(async () => {
+    await emptyDbTables(['Agency', 'AgencyToken'])
+  })
+})
+
 after(async () => {
   await sequelize.close()
 })
