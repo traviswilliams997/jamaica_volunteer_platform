c12d2d0f049236dca2c3b9507461342290b51491
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-02-28 16:11:28 -0500

    Add refresh token ability for agency

---
+++ server/controllers/refresh.js
@@ -0,0 +1,62 @@
+import jwt from 'jsonwebtoken'
+import { ACCESS_SECRET, REFRESH_SECRET } from '../utils/config.js'
+import {
+  Agency,
+  AgencyToken,
+  Volunteer,
+  VolunteerToken,
+} from '../models/index.js'
+
+export const refreshVolunteerAccessToken = async (req, res) => {
+  const cookies = req.cookies
+  if (!cookies?.jwt) return res.sendStatus(401)
+  const refreshToken = cookies.jwt
+
+  const responseToken = await VolunteerToken.findAll({
+    where: { token: refreshToken },
+    include: {
+      model: Volunteer,
+    },
+  })
+  const foundVolunteer = responseToken[0].volunteer
+  if (!foundVolunteer) return res.sendStatus(403)
+
+  jwt.verify(refreshToken, REFRESH_SECRET, (err, decoded) => {
+    if (err || foundVolunteer.username !== decoded.username)
+      return res.sendStatus(403)
+
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      ACCESS_SECRET,
+      { expiresIn: '600s' }
+    )
+    res.json({ accessToken })
+  })
+}
+
+export const refreshAgencyAccessToken = async (req, res) => {
+  const cookies = req.cookies
+  if (!cookies?.jwt) return res.sendStatus(401)
+  const refreshToken = cookies.jwt
+
+  const responseToken = await AgencyToken.findAll({
+    where: { token: refreshToken },
+    include: {
+      model: Agency,
+    },
+  })
+  const foundAgency = responseToken[0].agency
+  if (!foundAgency) return res.sendStatus(403)
+
+  jwt.verify(refreshToken, REFRESH_SECRET, (err, decoded) => {
+    if (err || foundAgency.username !== decoded.username)
+      return res.sendStatus(403)
+
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      ACCESS_SECRET,
+      { expiresIn: '600s' }
+    )
+    res.json({ accessToken })
+  })
+}
