0f4310685ed014646ab753fe7a1bac805182eb08
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-02-29 00:14:52 -0500

    Create home page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,31 @@
+import { Box, useMediaQuery } from '@mui/material'
+import { useSelector } from 'react-redux'
+import Navbar from '../navbar'
+import UserWidget from '../widgets/UserWidget'
+
 const HomePage = () => {
-  return <div>homepage</div>
+  const isNonMobileScreens = useMediaQuery('(min-width:1000px)')
+  const volunteer = useSelector((state) => state.volunteer.currentVolunteer)
+  return (
+    <Box>
+      {' '}
+      <Navbar />{' '}
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? 'flex' : 'block'}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget
+            volunteerId={volunteer.id}
+            picturePath={volunteer.picturePath}
+          />
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default HomePage
