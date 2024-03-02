e7c9a00eee40707303546783688bd41829e71ac0
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-03-02 12:09:47 -0500

    Redo last migrations

--- server/migrations/20240302_00_rename_created_by_agency_id_column_to_agency_id_column.js
+++ server/migrations/20240302_00_rename_created_by_agency_id_column_to_agency_id_column.js
@@ -2,9 +2,6 @@ import { DataTypes } from 'sequelize'
 
 const up = async ({ context: queryInterface }) => {
   await queryInterface.dropTable('positions')
-}
-
-const down = async ({ context: queryInterface }) => {
   await queryInterface.createTable('positions', {
     id: {
       type: DataTypes.INTEGER,
@@ -45,5 +42,49 @@ const down = async ({ context: queryInterface }) => {
       allowNull: false,
     },
   })
+}
+
+const down = async ({ context: queryInterface }) => {
+  await queryInterface.createTable('positions', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    created_by_agency_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'agencies', key: 'id' },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    skills: {
+      type: DataTypes.TEXT,
+    },
+    schedule: {
+      type: DataTypes.TEXT,
+    },
+    vacancies: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+  })
+  await queryInterface.dropTable('positions')
 }
 export { up, down }
